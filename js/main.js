eee62e5868f43a003da9a6a0ea557cb6ffd48372
wooohyun <woodoll543@gmail.com>
2022-07-13 17:48:24 +0900

    20220713

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(function () {
+$("#main_title_video").YTPlayer({
+    videoURL: "https://youtu.be/GQBQvb6miuo",
+    containment: ".main_title_video",
+    autoPlay: true,
+    mute: true,
+    startAt: 0,
+    opacity: 1,
+    showControls: false,
+    playOnlyIfVisible: true,
+    loop:true,
+    // 화질설정
+    quality:'hd1080'
+  });
+
+  $('.main_slide .container').slick({
+    slidesToShow: 3,
+});
+
+
+});
