5c1045a9c1c38d6f4752b206e42cdf0be7e18e9f
wooohyun <woodoll543@gmail.com>
2022-10-24 15:22:01 +0900

    20221024

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(function () {
-$("#main_title_video").YTPlayer({
+  $("#main_title_video").YTPlayer({
     videoURL: "https://youtu.be/GQBQvb6miuo",
     containment: ".main_title_video",
     autoPlay: true,
@@ -8,18 +8,18 @@ $("#main_title_video").YTPlayer({
     opacity: 1,
     showControls: false,
     playOnlyIfVisible: true,
-    loop:true,
+    loop: true,
     // 화질설정
-    quality:'hd1080'
+    quality: 'hd1080'
   });
 
   $('.main_slide .container').slick({
     slidesToShow: 3,
-});
+  });
 
   $('.main_story .container').slick({
     slidesToShow: 3,
-});
+  });
 
 
 });
